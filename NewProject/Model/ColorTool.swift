ddebb1dcc5a6c0d29c1b896558577edb8268c279
Cy Ellison <dragonz2626@gmail.com>
2018-10-09 12:20:39 -0600

    added model group and renamed files

---
+++ NewProject/Model/ColorTool.swift
@@ -0,0 +1,15 @@
+//
+//  ColorTool.swift
+//  NewProject
+//
+//  Created by Ellison, Cy on 10/9/18.
+//  Copyright © 2018 Ellison, Cy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    
+}
